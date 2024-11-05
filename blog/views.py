0e354b9df756ae09ad63bd247162cbb81268ce37
yas3in <jastinafa2016@gmail.com>
2024-11-05 21:52:07 +0330

    handle show blog objectdoesnotexist

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from blog.models import Blog
 
 
@@ -12,5 +14,10 @@ def blog_list(request):
 
 def show_blog(request, content):
     context = {}
-    context['show_blog'] = Blog.objects.filter(content__icontains=content, content__contains=content)[:1].get()
-    return render(request, "blog/show_blog.html", context=context) 
+    
+    try:
+        context['show_blog'] = Blog.objects.filter(content__icontains=content, content__contains=content)[:1].get()
+        if context['show_blog']:
+            return render(request, 'blog/show_blog.html', context=context)   
+    except ObjectDoesNotExist:
+        return render(request, 'blog/blog_list.html') 
