312fc765b64ce722986de3a3a52ac25e2e729979
yas3in <jastinafa2016@gmail.com>
2024-11-06 17:31:21 +0330

    edit blog_list page and get style to this

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.db.models import Q
+
 from django.core.exceptions import ObjectDoesNotExist
 
 from blog.models import Blog
@@ -16,7 +18,7 @@ def show_blog(request, content):
     context = {}
     
     try:
-        context['show_blog'] = Blog.objects.filter(content__icontains=content, content__contains=content)[:1].get()
+        context['show_blog'] = Blog.objects.filter(Q(title__icontains=content) | Q(content__icontains=content))[:1].get()
         if context['show_blog']:
             return render(request, 'blog/show_blog.html', context=context)   
     except ObjectDoesNotExist:
