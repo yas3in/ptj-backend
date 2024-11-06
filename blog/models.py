594ab9b7e7c14153991970c6d7339470667662a7
yas3in <jastinafa2016@gmail.com>
2024-11-06 19:11:43 +0330

    crate test folder and install jdatetime library and use in blog models

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from main.models import User
 import datetime
+import jdatetime
 
 
 class Blog(models.Model):
+    date = jdatetime.datetime.now()
+    date_str = date.strftime("%Y/%m/%d")
     now = datetime.datetime.now()
     auther = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     title = models.CharField (max_length=48)
     content = models.TextField()
-    date = models.DateField(auto_now=now)
+    date = models.DateField(auto_now=date_str)
     
     def __str__(self):
         return f"{self.title}"
