c050d38a927949318a9155b92b6584c2b1643120
yas3in <jastinafa2016@gmail.com>
2024-11-07 18:33:10 +0330

    add 404 pages and complete this

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import index, aboutus, contact
+from main.views import index, aboutus, contact, service
 
 urlpatterns = [
     path("", index, name="index"),
     path("درباره-ما", aboutus, name="aboutus"),
     path("ارتباط-با-ما", contact, name="contact"),
+    path("خدمات-ما", service, name="404"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main/index.html')
 
+
 def aboutus(request):
     return render(request, "main/aboutus.html")
 
+
 def contact(request):
     return render(request, "main/contact.html")
+
+
+def service(request):
+    return render(request, "404.html")
