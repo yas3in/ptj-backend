eb2016e819216941cd9ef1af52906d2d661457af
yas3in <jastinafa2016@gmail.com>
2024-11-11 21:16:28 +0330

    add local_setting in repo

---
+++ ptj/local_setting.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rj_i-g6qa9d+ha2&m!-@8$!wq6_ym&0g9rq6ts!@sq9icm$(*%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
