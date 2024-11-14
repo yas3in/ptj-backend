bdff427dd43825693041535290ebe6357d541deb
yas3in <jastinafa2016@gmail.com>
2024-11-14 17:09:18 +0330

    add urlpatterns

--- ptj/urls.py
+++ ptj/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include("main.urls")),
 ]
+
+urlpatterns = urlpatterns + static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
